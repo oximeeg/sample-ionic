473e94b5276a8979094a2805b0b04a5a53af30c2
oximeeg <oximeeg@gmail.com>
2021-01-05 03:55:08 +0900

    add social-sharing (#2)

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,9 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private socialSharing: SocialSharing) {}
 
+  onClick(){
+    this.socialSharing.share('The message you would like to share');
+  }
 }
